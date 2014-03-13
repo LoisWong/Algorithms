5f2613f81e00054a135f99417633befb2c2a198d
cat715 <bianguanlin007@gmail.com>
2014-03-13 09:08:40 +0800

    finish Dequeue

--- Queues/src/Deque.java
+++ Queues/src/Deque.java
@@ -1,4 +1,69 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Deque {
-
+public class Deque<Item> implements Iterable<Item> {
+    private List<Item> list;
+    // construct an empty deque
+    public Deque() {
+       list = new LinkedList<Item>();
+    }
+    // is the deque empty?
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+    // return the number of items on the deque
+    public int size() {
+        return list.size();
+    }
+    // insert the item at the front
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new NullPointerException("");
+        }
+        list.add(0, item);
+    }
+    // insert the item at the end
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new NullPointerException("");
+        }
+        list.add(item);
+    }
+    // delete and return the item at the front
+    public Item removeFirst() {
+        if (isEmpty()) 
+            throw new java.util.NoSuchElementException();
+        return list.remove(0);
+    }
+    // delete and return the item at the end
+    public Item removeLast() {
+        if (isEmpty()) 
+            throw new java.util.NoSuchElementException();
+        return list.remove(list.size() - 1);
+    }
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return this.new DequeIterator<Item>();
+    }
+    
+    private class DequeIterator<E> implements Iterator<E> {
+        public boolean hasNext() {
+            return size() > 0;
+        }
+        public E next() {
+            if (isEmpty()) {
+                throw new java.util.NoSuchElementException();
+            }
+            return (E) removeFirst();
+        }
+        public void remove() {
+            throw new UnsupportedOperationException();      
+        }
+        
+    }
+    // unit testing
+    public static void main(String[] args) {
+        return;
+    }
 }
